5d4eeb52ff744c32dcb24efc7b6eda0df78eb0da
Thiago <tihsouza01@gmail.com>
2025-08-15 17:13:47 -0300

    Adicionando um usuario a tabela de funcionarios

---
+++ db2-cobol-dml.sql
@@ -0,0 +1,8 @@
+000001   INSERT INTO FUNCIONARIOS             
+000002       VALUES('AA01',                   
+000003              'THIAGO SOUZA',           
+000004              1210.45,                  
+000005              'ADM',                    
+000006              '08/15/2025',             
+000007              23,                       
+000008              'THIAGO@TESTE.COM')       
