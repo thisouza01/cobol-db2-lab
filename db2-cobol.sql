ec91c1bb419f3f6383bdbcdadb7611e225b3b974
Thiago <tihsouza01@gmail.com>
2025-08-15 16:51:46 -0300

    Criando tabela de funcionarios que tera um campo que posteriomente sera associado a outra tabela

--- db2-cobol.sql
+++ db2-cobol.sql
@@ -4,3 +4,12 @@
 000004 --------------------------------------------------
 000005 --DROP   TABLE DEPARTAMENTOS                      
 000006 --------------------------------------------------
+000007   CREATE TABLE FUNCIONARIOS                       
+000008       (CODFUN       CHAR(4) PRIMARY KEY NOT NULL, 
+000009        NOMEFUN      VARCHAR(30)         NOT NULL, 
+000010        SALARIOFUN   DECIMAL(8,2)        NOT NULL, 
+000011        DEPTOFUN     CHAR(3)             NOT NULL, 
+000012        ADMISSFUN    DATE                NOT NULL, 
+000013        IDADEFUN     SMALLINT            NOT NULL, 
+000014        EMAILFUN     VARCHAR(30))                  
+000015 --------------------------------------------------
