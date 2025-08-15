23288e5f00df3059520b77df38e49b80b8ca6a32
Thiago <tihsouza01@gmail.com>
2025-08-15 16:56:42 -0300

    Criando indices que facilitaram a pesquisa em cada tabela, melhorando a performance

--- db2-cobol.sql
+++ db2-cobol.sql
@@ -4,12 +4,17 @@
 000004 --------------------------------------------------
 000005 --DROP   TABLE DEPARTAMENTOS                      
 000006 --------------------------------------------------
-000007   CREATE TABLE FUNCIONARIOS                       
-000008       (CODFUN       CHAR(4) PRIMARY KEY NOT NULL, 
-000009        NOMEFUN      VARCHAR(30)         NOT NULL, 
-000010        SALARIOFUN   DECIMAL(8,2)        NOT NULL, 
-000011        DEPTOFUN     CHAR(3)             NOT NULL, 
-000012        ADMISSFUN    DATE                NOT NULL, 
-000013        IDADEFUN     SMALLINT            NOT NULL, 
-000014        EMAILFUN     VARCHAR(30))                  
+000007 --CREATE TABLE FUNCIONARIOS                       
+000008 --    (CODFUN       CHAR(4) PRIMARY KEY NOT NULL, 
+000009 --     NOMEFUN      VARCHAR(30)         NOT NULL, 
+000010 --     SALARIOFUN   DECIMAL(8,2)        NOT NULL, 
+000011 --     DEPTOFUN     CHAR(3)             NOT NULL, 
+000012 --     ADMISSFUN    DATE                NOT NULL, 
+000013 --     IDADEFUN     SMALLINT            NOT NULL, 
+000014 --     EMAILFUN     VARCHAR(30))                  
 000015 --------------------------------------------------
+000016   CREATE UNIQUE INDEX IDXFUNC                     
+000017       ON FUNCIONARIOS(CODFUN);                    
+000018   CREATE UNIQUE INDEX IDXDEPTO                    
+000019       ON DEPARTAMENTOS(CODDEPTO)                  
+000020 --------------------------------------------------
