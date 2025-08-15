9322221dc0c16070a29ea5bc686070fc77c462ca
Thiago <tihsouza01@gmail.com>
2025-08-15 16:48:55 -0300

    Excluindo tabela criada anteriormente

--- db2-cobol.sql
+++ db2-cobol.sql
@@ -1,3 +1,6 @@
- 000001 --CREATE TABLE DEPARTAMENTOS                       
- 000002 --    (CODDEPTO     CHAR(3) PRIMARY KEY NOT NULL,  
- 000003 --     NOMEDEPTO    VARCHAR(30)         NOT NULL)  
+000001 --CREATE TABLE DEPARTAMENTOS                       
+000002 --    (CODDEPTO     CHAR(3) PRIMARY KEY NOT NULL,  
+000003 --     NOMEDEPTO    VARCHAR(30)         NOT NULL)  
+000004 --------------------------------------------------
+000005 --DROP   TABLE DEPARTAMENTOS                      
+000006 --------------------------------------------------
