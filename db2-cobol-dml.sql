2949aa8667381019b3174b06e9f0f40a7a4d871a
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-08-19 08:12:15 -0300

    add: Valores para a tabela de departamentos.

--- db2-cobol-dml.sql
+++ db2-cobol-dml.sql
@@ -1,8 +1,11 @@
-000001   INSERT INTO FUNCIONARIOS             
-000002       VALUES('AA01',                   
-000003              'THIAGO SOUZA',           
-000004              1210.45,                  
-000005              'ADM',                    
-000006              '08/15/2025',             
-000007              23,                       
-000008              'THIAGO@TESTE.COM')       
+000001 --INSERT INTO FUNCIONARIOS             
+000002 --  VALUES('AA01',                   
+000003 --         'THIAGO SOUZA',           
+000004 --         1210.45,                  
+000005 --         'ADM',                    
+000006 --         '08/15/2025',             
+000007 --         23,                       
+000008 --         'THIAGO@TESTE.COM')      
+000009 --INSERT INTO DEPARTAMENTOS   
+000010 --    VALUES('ADM',           
+000011 --           'ADMINISTRACAO') 
