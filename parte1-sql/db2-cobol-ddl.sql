72f15c88c6fd36174972986c74dd66fd6b0904b3
Thiago <tihsouza01@gmail.com>
2025-08-20 10:58:41 -0300

    alter: mudando para outra pasta, melhorar legitibilidade do programa

---
+++ parte1-sql/db2-cobol-ddl.sql
@@ -0,0 +1,25 @@
+000001 --CREATE TABLE DEPARTAMENTOS                       
+000002 --    (CODDEPTO     CHAR(3) PRIMARY KEY NOT NULL,  
+000003 --     NOMEDEPTO    VARCHAR(30)         NOT NULL)  
+000004 --------------------------------------------------
+000005 --DROP   TABLE DEPARTAMENTOS                      
+000006 --------------------------------------------------
+000007 --CREATE TABLE FUNCIONARIOS                       
+000008 --    (CODFUN       CHAR(4) PRIMARY KEY NOT NULL, 
+000009 --     NOMEFUN      VARCHAR(30)         NOT NULL, 
+000010 --     SALARIOFUN   DECIMAL(8,2)        NOT NULL, 
+000011 --     DEPTOFUN     CHAR(3)             NOT NULL, 
+000012 --     ADMISSFUN    DATE                NOT NULL, 
+000013 --     IDADEFUN     SMALLINT            NOT NULL, 
+000014 --     EMAILFUN     VARCHAR(30))                  
+000015 --------------------------------------------------
+000016 --CREATE UNIQUE INDEX IDXFUNC                     
+000017 --    ON FUNCIONARIOS(CODFUN);                    
+000018 --CREATE UNIQUE INDEX IDXDEPTO                    
+000019 --    ON DEPARTAMENTOS(CODDEPTO)                  
+000020 --------------------------------------------------
+000021   ALTER TABLE FUNCIONARIOS                        
+000022       ADD FOREIGN KEY(DEPTOFUN)                   
+000023       REFERENCES DEPARTAMENTOS                    
+000024       ON DELETE RESTRICT                          
+000025 --------------------------------------------------
